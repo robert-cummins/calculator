54379e4f0193a6cb879c60b2793775c145ae7771
robert-cummins <robert.k.cummins@gmail.com>
2019-09-24 10:25:53 +1200

    Add pseudocode

---
+++ calculator.js
@@ -0,0 +1,16 @@
+//Create a calculator object with entries array, total set to 0 and temp that is an empty string.
+
+//Create an event listener to check for what key is pressed
+
+//If the key is a number or decimal point, add to temp and display to screen. Limit to 10 didgets.
+
+//If the key is AC, reset all values from calculator back to origional
+
+//If the key is CE, reset the temp string and clear display
+
+//If key pushed is X, Push temp and * to entries, display temp
+
+//If key is ÷, push temp and / to entries, display temp
+
+//Write a function that evaluates the entires array
+//loop through the array. Convert string to number. Check what symbol and apply to next number. New varibales may be needed for this.
