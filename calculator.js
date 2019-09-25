474788e54f9484c5ebfdf45c74f71333cb372b54
robert-cummins <robert.k.cummins@gmail.com>
2019-09-26 11:46:52 +1200

    Add evaluate function

--- calculator.js
+++ calculator.js
@@ -10,19 +10,29 @@ let screen = document.querySelector('.display');
 //Create an event listener to check for what key is pressed
 document.addEventListener('click', function (event) {
     let val = event.target.value;
-    console.log(event.target.type);
+
     //If the key is a number or decimal point, add to temp and display to screen. Limit to 10 didgets.
     if (!(isNaN(val)) || val === '.') {
         calculator.temp += val;
         screen.value = calculator.temp.substring(0, 10);
+        //Handle operators    
     } else if (val === '÷') {
         operator('÷');
-    } else if (val === 'x'){
-        calculator.entires.push(calculator.temp);
+    } else if (val === 'x') {
+        operator('x');
+    } else if (val === '+') {
+        operator('+');
+    } else if (val === '-') {
+        operator('-');
+        //Handle Cancle button    
+    } else if (val === 'C') {
+        calculator.entires = [];
+        calculator.total = 0;
         calculator.temp = '';
-        val = '*';
-        calculator.entires.push(val);
         screen.value = '';
+    //Handle evaluate button    
+    } else if (val === '=') {
+        evaluate(calculator.entires);
     }
 
 });
@@ -42,13 +52,30 @@ function operator(op) {
     screen.value = '';
 }
 
-//If the key is AC, reset all values from calculator back to origional
-
-//If the key is CE, reset the temp string and clear display
-
-//If key pushed is X, Push temp and * to entries, display temp
-
-//If key is ÷, push temp and / to entries, display temp
 
 //Write a function that evaluates the entires array
 //loop through the array. Convert string to number. Check what symbol and apply to next number. New varibales may be needed for this.
+function evaluate(arr) {
+    calculator.entires.push(calculator.temp);
+    calculator.total = parseInt(arr[0]);
+    for (i = 0; i < arr.length; i++){
+        
+        if (!(isNaN(arr[i]))) {
+            if (arr[i + 1] === '+') {
+                calculator.total += parseInt(arr[i + 2]);
+            } else if (arr[i + 1] === '-') {
+                calculator.total -= parseInt(arr[i + 2]);
+            } else if (arr[i + 1] === '/') {
+                calculator.total /= parseInt(arr[i + 2]);
+            } else if (arr[i + 1] === '*') {
+                calculator.total *= parseInt(arr[i + 2]);
+            }
+        }
+    }
+    //console.log(calculator.total)
+    calculator.entires = [];
+    calculator.entires.push(calculator.total);
+    calculator.temp = calculator.total;
+    //console.log(calculator.entires);
+    screen.value = calculator.total;
+}
