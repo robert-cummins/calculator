1bd3440638b9cc43eebf81258ae5a35b4674c33c
robert-cummins <robert.k.cummins@gmail.com>
2019-09-25 18:35:28 +1200

    Add calculator design and calculator object

--- calculator.js
+++ calculator.js
@@ -1,8 +1,26 @@
 //Create a calculator object with entries array, total set to 0 and temp that is an empty string.
+let calculator = {
+    entires: [],
+    total: 0,
+    temp: ''
+}
+
+let screen = document.querySelector('.display');
 
 //Create an event listener to check for what key is pressed
+document.addEventListener('click', function (event) {
+    let val = event.target.value;
+    console.log(event.target.type);
+    //If the key is a number or decimal point, add to temp and display to screen. Limit to 10 didgets.
+    if (!(isNaN(val)) || val === '.') {
+        calculator.temp += val;
+        screen.value = calculator.temp.substring(0, 10);
+    } else if (val === '÷') {
+        val = '/'
+    }
 
-//If the key is a number or decimal point, add to temp and display to screen. Limit to 10 didgets.
+});
+
 
 //If the key is AC, reset all values from calculator back to origional
 
